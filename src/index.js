2ab35975e419f2f4f83bb88977dfb1023b6ceb9c
Jalen Adams <jalen@jalenkadams.me>
2020-09-04 19:26:09 -0400

    Add: page header

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log('Hello world!');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function Header(props) {
+  const { text } = props;
+  return <h1>{text}</h1>;
+}
+
+ReactDOM.render(
+  <Header text="Julianne's Breakfast Cafe" />,
+  document.getElementById('root'),
+);
