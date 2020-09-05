cf2d5f67cb025cf9f2ed6ca5d06a059bcb13a818
Jalen Adams <jalen@jalenkadams.me>
2020-09-04 20:36:11 -0400

    Add: tab bar

--- src/index.js
+++ src/index.js
@@ -6,7 +6,35 @@ function Header(props) {
   return <h1>{text}</h1>;
 }
 
-ReactDOM.render(
-  <Header text="Julianne's Breakfast Cafe" />,
-  document.getElementById('root'),
-);
+function TabButton(props) {
+  let { text } = props;
+  text = String(text);
+
+  return (
+    <div>
+      <input type="radio" name="tabBar" value={text.toLowerCase()} />
+      <label htmlFor={text.toLowerCase()}>{text}</label>
+    </div>
+  );
+}
+
+function TabBar() {
+  return (
+    <div>
+      <TabButton text="Home" />
+      <TabButton text="Menu" />
+      <TabButton text="Contact" />
+    </div>
+  );
+}
+
+function HomePage() {
+  return (
+    <div>
+      <Header text="Julianne's Breakfast Cafe" />
+      <TabBar />
+    </div>
+  );
+}
+
+ReactDOM.render(<HomePage />, document.getElementById('root'));
