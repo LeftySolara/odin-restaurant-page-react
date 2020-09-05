f88328643e871d749e5384894f1e678f4d49cbc5
Jalen Adams <jalen@jalenkadams.me>
2020-09-04 20:40:29 -0400

    Add: restaurant info

--- src/index.js
+++ src/index.js
@@ -28,11 +28,26 @@ function TabBar() {
   );
 }
 
+function RestaurantInfo() {
+  const paragraph1 =
+    "Located in the heart of the arts district, Julianne's Cafe is the perfect spot for a relaxing breakfast atmosphere.";
+  const paragraph2 =
+    'Established in 2020, our cafe strives to provide the best traditional, experimental, and fun options for the most important meal of the day!';
+
+  return (
+    <div>
+      <p>{paragraph1}</p>
+      <p>{paragraph2}</p>
+    </div>
+  );
+}
+
 function HomePage() {
   return (
     <div>
       <Header text="Julianne's Breakfast Cafe" />
       <TabBar />
+      <RestaurantInfo />
     </div>
   );
 }
