81d8a275a43fde4bfc4ee07e67cf08f398d345f6
Jalen Adams <jalen@jalenkadams.me>
2020-09-05 07:09:00 -0400

    Add: contact info component

--- src/index.js
+++ src/index.js
@@ -43,6 +43,25 @@ function RestaurantInfo() {
   );
 }
 
+function ContactInfo() {
+  const address = '12345 Example St., Seattle, WA';
+  const email = 'julianne@example.com';
+  const phone = '(123)456-7890';
+
+  return (
+    <div className="tabContent overlay">
+      <p>For questions, concerns, and reservations:</p>
+      <p />
+      <p>{address}</p>
+      <p>
+        {email}
+        <br />
+        {phone}
+      </p>
+    </div>
+  );
+}
+
 function HomePage() {
   return (
     <div>
