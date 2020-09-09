874dd3dbdb4841d1b8d124e9fdac6069dded3504
Jalen Adams <jalen@jalenkadams.me>
2020-09-08 20:50:53 -0400

    Updates

---
+++ precache-manifest.c7793029660d0d62d9790c88be548eaf.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dec5d55e073249bed23e77aef5263fe",
+    "url": "/odin-restaurant-page-react/index.html"
+  },
+  {
+    "revision": "73f8778c0669c1693d29",
+    "url": "/odin-restaurant-page-react/static/css/main.ef9cf30f.chunk.css"
+  },
+  {
+    "revision": "8fafa97399adab2c0547",
+    "url": "/odin-restaurant-page-react/static/js/2.3f40c86d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/odin-restaurant-page-react/static/js/2.3f40c86d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73f8778c0669c1693d29",
+    "url": "/odin-restaurant-page-react/static/js/main.feae035a.chunk.js"
+  },
+  {
+    "revision": "a65e9143ba210bf559ee",
+    "url": "/odin-restaurant-page-react/static/js/runtime-main.93aff653.js"
+  },
+  {
+    "revision": "69e574c39a6baa876dbf09f65663d72a",
+    "url": "/odin-restaurant-page-react/static/media/background.69e574c3.jpg"
+  },
+  {
+    "revision": "ef580cba1317ec0887eeb4a69f62fb95",
+    "url": "/odin-restaurant-page-react/static/media/bagel.ef580cba.jpg"
+  },
+  {
+    "revision": "1a57dc5af70c321dedbfca468fdc0154",
+    "url": "/odin-restaurant-page-react/static/media/banana_nut_oatmeal.1a57dc5a.jpg"
+  },
+  {
+    "revision": "9fa4ccd4809f093e51307e2ce950b333",
+    "url": "/odin-restaurant-page-react/static/media/blueberry_pancakes.9fa4ccd4.jpg"
+  },
+  {
+    "revision": "f0e23c87ac978186541cc2548ac6cfa2",
+    "url": "/odin-restaurant-page-react/static/media/blueberry_waffles.f0e23c87.jpg"
+  },
+  {
+    "revision": "45c52478976433812cc05809edee0885",
+    "url": "/odin-restaurant-page-react/static/media/breakfast_platter.45c52478.jpg"
+  },
+  {
+    "revision": "cca65f656b0aca3ee3bd649bf3fd1f50",
+    "url": "/odin-restaurant-page-react/static/media/coffee.cca65f65.jpg"
+  },
+  {
+    "revision": "285ace60a945eac420a70fe43eb7d844",
+    "url": "/odin-restaurant-page-react/static/media/egg_on_toast.285ace60.jpg"
+  },
+  {
+    "revision": "e07f64eb49f263b903f570ed17682496",
+    "url": "/odin-restaurant-page-react/static/media/french_toast.e07f64eb.jpg"
+  },
+  {
+    "revision": "2d71ef4a7fd18844e5a96ab8c18d1873",
+    "url": "/odin-restaurant-page-react/static/media/tea.2d71ef4a.jpg"
+  }
+]);
